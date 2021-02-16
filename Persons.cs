87f8556831d9e838afeda58f4cc57a7634fa3bcb
mohamedslam <mohamedslam2000@yahoo.com>
2021-02-16 14:47:31 +0500

    Add project files.

---
+++ Persons.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FamilyStructure_2
+{
+    class Person
+    {
+
+        #region Feilds
+
+        public string FullName { get; }
+        private Person Husband { get; set; }
+        private Person ParantRecord { get; }
+
+        private List<Person> MainParant_list = new List<Person>();
+        private List<Person> Parant_Child_list = new List<Person>();
+
+        #endregion
+
+        #region Methods
+        public Person(string name, Person parent = null, Person spouse = null)
+        {
+            this.FullName = name;
+            this.ParantRecord = parent;
+            this.Husband = spouse;
+
+            if (parent != null)
+            {
+                this.ParantRecord.Parant_Child_list.Add(this);
+                this.MainParant_list.Add(this.ParantRecord);
+                if (this.ParantRecord.Husband != null)
+                {
+                    this.ParantRecord.Husband.Parant_Child_list.Add(this);
+                    this.MainParant_list.Add(this.ParantRecord.Husband);
+                }
+            }
+        }
+
+        public List<Person> GetAllChilds()
+        {
+            return this.Parant_Child_list;
+        }
+
+        public List<Person> GetMainParents()
+        {
+            return this.MainParant_list;
+        }
+
+        public List<Person> GetBrothersAndSisters()
+        {
+            List<Person> Brothers_list = new List<Person>();
+            if (this.ParantRecord != null)
+            {
+                foreach (Person parent in this.GetMainParents())
+                {
+                    foreach (Person sibling in parent.GetAllChilds())
+                    {
+                        if (sibling != null)
+                        {
+                            if (sibling != this)
+                            {
+                                if (!Brothers_list.Contains(sibling))
+                                    Brothers_list.Add(sibling);
+                            }
+                        }
+
+                    }
+                }
+                return Brothers_list;
+            }
+            else
+                return null;
+
+        }
+
+        public List<Person> GetAllUncles()
+        {
+            List<Person> AllUncles_list = new List<Person>();
+            if (this.ParantRecord != null)
+            {
+                foreach (Person parent in this.GetMainParents())
+                {
+                    if (parent.ParantRecord != null)
+                    {
+                        foreach (Person uncle in parent.GetBrothersAndSisters())
+                        {
+                            if (uncle != null)
+                            {
+                                if (!AllUncles_list.Contains(uncle))
+                                    AllUncles_list.Add(uncle);
+                                if (uncle.Husband != null)
+                                {
+                                    if (!AllUncles_list.Contains(uncle.Husband))
+                                        AllUncles_list.Add(uncle.Husband);
+                                }
+                            }
+                        }
+                    }
+                }
+                return AllUncles_list;
+            }
+            else
+                return null;
+        }
+
+        public List<Person> GetCousins()
+        {
+            List<Person> cousins_list = new List<Person>();
+            if (this.ParantRecord != null)
+            {
+                foreach (Person uncle in this.GetAllUncles())
+                {
+                    if (uncle != null)
+                    {
+                        foreach (Person cousine in uncle.GetAllChilds())
+                        {
+                            if (cousine != null)
+                            {
+                                if (!cousins_list.Contains(cousine))
+                                    cousins_list.Add(cousine);
+                            }
+                        }
+                    }
+
+                }
+                return cousins_list;
+            }
+            else
+                return null;
+        }
+
+        public List<Person> GetInLaw()
+        {
+            List<Person> inlaw_list = new List<Person>();
+            if (Husband != null)
+            {
+                if (Husband.ParantRecord != null)
+                {
+                    foreach (Person inlaw in Husband.GetMainParents())
+                    {
+                        if (inlaw != null)
+                            inlaw_list.Add(inlaw);
+                    }
+                }
+                return inlaw_list;
+            }
+            else
+                return null;
+        }
+
+        public static void Married(Person a, Person b)
+        {
+            if (a == b)
+                Console.WriteLine(a.FullName + " Its Not Logical");
+            else if (a.Husband != null)
+                Console.WriteLine(a.FullName + " has a Husband - " + a.Husband.FullName);
+            else if (b.Husband != null)
+                Console.WriteLine(b.FullName + " has a Husband - " + b.Husband.FullName);
+            else
+            {
+                a.Husband = b;
+                b.Husband = a;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyStructure_2
+{
+    class Program
+    {
+        static List<string> GetNames(List<Person> Elements)
+        {
+            List<string> names = new List<string>();
+            if (Elements.Count != 0)
+            {
+                foreach (Person elm in Elements)
+                {
+                    if (elm != null)
+                        names.Add(elm.FullName);
+                }
+                return names;
+            }
+            else
+                return null;
+
+        }
+        static void PrintNames(List<Person> Elements)
+        {
+            if (Elements.Count != 0)
+            {
+                foreach (Person elm in Elements)
+                {
+                    if (elm != null)
+                        Console.Write(elm.FullName + " ");
+                }
+            }
+            else
+                Console.Write("No Data Elments Found");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+
+            //<<<<<<<<<<<< Family Ftructure >>>>///////////////////////
+
+            //level 1 of familyStructure 
+            Person Mohamed = new Person("Mohamed");
+            Person Ahmed = new Person("Ahmed");
+            Person.Married(Mohamed, Ahmed);
+
+            Person Yousry = new Person("Yousry");
+            Person khaled = new Person("khaled");
+
+            Person.Married(Yousry, khaled);
+
+            // level 2 of familyStructure 
+            Person Azziz = new Person("Azziz", Mohamed);
+            Person Mona = new Person("Mona", Mohamed);
+
+            Person Yousf = new Person("Yousf", Yousry);
+
+            Person AzzizWife = new Person("AzzizWife");
+
+            Person.Married(Mona, Yousf);
+            Person.Married(Azziz, AzzizWife);
+
+            //level 3 of familyStructure 
+            Person Egor = new Person("Egor", Mona);
+            Person Ivan = new Person("Ivan", Mona);
+            Person Youri = new Person("Youri", Mona);
+
+            Person Bakar = new Person("Bakar", Azziz);
+            Person AbdAlrhman = new Person("AbdAlrhman", Azziz);
+            Person Ismail = new Person("Ismail", Azziz);
+            
+
+
+            /////////////////////////// testing Structure//////////////////////////////////
+            ///
+            Person Amal = new Person("Amal");
+            Person.Married(Amal, Amal);
+
+            Person Ebrahim = new Person("Ebrahim");
+            Person.Married(Amal, Ebrahim);
+            Person Mechaeil = new Person("Mechaeil");
+            Person.Married(Amal, Mechaeil);
+
+
+            ///////Printing Data
+            PrintNames(Mechaeil.GetMainParents());
+            PrintNames(Youri.GetMainParents());
+            PrintNames(Ivan.GetAllUncles());
+            PrintNames(Bakar.GetCousins());
+            PrintNames(Mona.GetInLaw());
+            PrintNames(Mona.GetMainParents());
+            PrintNames(Mona.GetMainParents());
+            PrintNames(Youri.GetCousins());
+            PrintNames(Youri.GetCousins());
+
+            Console.ReadLine();
+        }
+
+    }
+}
